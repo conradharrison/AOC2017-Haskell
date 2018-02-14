c3f4eca4af4cd4b58259883bca87dcc98eda3cef
Conrad Harrison <conrad.harrison@gmail.com>
2018-02-14 23:00:33 +0530

    Working 9

--- src/9.hs
+++ src/9.hs
@@ -1,12 +1,11 @@
 import Prelude hiding (EQ, GT, LT)
-import System.Environment ( getArgs )
-import Debug.Trace
+import System.Environment (getArgs)
 
 -- Helper class
 data Stack a = Stack [a] deriving Show
 
 push :: (Show a) => Stack a -> a -> Stack a
-push (Stack s) c = Stack ((trace (show c) c):s)
+push (Stack s) c = Stack (c:s)
 
 pop :: Stack a -> (Maybe a, Stack a)
 pop (Stack []) = (Nothing, Stack [])
@@ -18,54 +17,34 @@ peek (Stack (x:xs)) = (Just x, Stack (x:xs))
 -- End Helpers
 
 data Token = SGRP | EGRP | SGARB | EGARB | IGNORENEXT | COMMA deriving (Eq, Enum, Show)
-type ParserState = State (Stack Token, Int)
 
-upLevel :: ParserState 
-upLevel = do
-            (s, l) <- get
-            return (ns, l-1) where (Just ns) = pop s
+type CurrentLevel = Int
+type GroupScore = Int
+type GarbageCount = Int
 
-downLevel :: ParserState 
-downLevel = do
-                (s, l) <- get
-                return (push s SGRP, l+1)
+-- Use a stack to parse the stream. Need current running level to accumulate
+parse :: Stack Token -> [Char] -> CurrentLevel -> GroupScore -> GarbageCount -> (GroupScore, GarbageCount)
+parse s []       l a g = (a, g)
+parse s (x:rest) l a g | (top == Just IGNORENEXT       ) = parse (let (_, ns) = pop s in ns) rest  l     a    g
+                       | (                     x == '!') = parse (push s IGNORENEXT)         rest  l     a    g
+                       | (top == Just SGARB && x == '>') = parse (push s EGARB)              rest  l     a    g
+                       | (top == Just SGARB            ) = parse       s                     rest  l     a   (g+1)
+                       | (top == Just EGARB && x == '{') = parse (push s SGRP)               rest (l+1)  a    g
+                       | (top == Just EGARB && x == '}') = parse (push s EGRP)               rest (l-1) (a+l) g
+                       | (top == Just EGARB && x == '<') = parse (push s SGARB)              rest  l     a    g
+                       | (top == Just SGRP  && x == '{') = parse (push s SGRP)               rest (l+1)  a    g
+                       | (top == Just SGRP  && x == '<') = parse (push s SGARB)              rest  l     a    g
+                       | (top == Just SGRP  && x == '}') = parse (push s EGRP)               rest (l-1) (a+l) g
+                       | (top == Just EGRP  && x == '{') = parse (push s EGRP)               rest (l+1)  a    g
+                       | (top == Just EGRP  && x == '}') = parse (push s EGRP)               rest (l-1) (a+l) g
+                       | (top == Just EGRP  && x == '<') = parse (push s SGARB)              rest  l     a    g
+                       | (top == Just EGRP  && x == '>') = parse (push s EGRP)               rest  l     a    g
+                       | (top == Nothing    && x == '{') = parse (push s SGRP)               rest (l+1)  a    g
+                       | otherwise                       = parse       s                     rest  l     a    g
+                       where (top, _) = peek s
 
-keepLevel :: ParserState
-keepLevel = do
-                (s, l) <- get
-                return (ns, l) where (Just ns) = pop s
-
-pushToken :: Token -> ParserState
-pushToken t = do
-                (s, l) <- get
-                return (push s t, l)
-
-parse :: [Char] -> Int -> (ParserState, Int)
-parse [] a       = (return (), a) 
-parse (x:rest) a | (top == Just IGNORENEXT       ) = parse (let (_, ns) = pop s in ns) rest
-                 | (top == Just SGARB && x == '>') = parse (push s EGARB) rest 
-                 | (top == Just EGARB && x == ',') = parse (push s COMMA) rest 
-                 | (top == Just EGARB && x == '}') = (downLevel, a)
-                 | (top == Just SGRP  && x == '<') = parse (push s SGARB) rest 
-                 | (top == Just SGRP  && x == '{') = (upLevel, a)
-                 | (top == Just SGRP  && x == '}') = (downLevel, a)
-                 | (top == Just EGRP  && x == '}') = (downLevel, a) 
-                 | (top == Just EGRP  && x == ',') = (pushToken COMMA, a)
-                 | (top == Just COMMA && x == '{') = (upLevel, a)
-                 | (top == Just COMMA && x == '<') = (pushToken SGARB, a) 
-                 | (top == Nothing    && x == '{') = (upLevel, a+1)
-                 | (                     x == '!') = parse (push s IGNORENEXT) rest 
-                 | otherwise                       = parse s rest 
-                 where (top, _) = peek s
-
-getScore :: Stack Token -> Int -> Int
-getScore (Stack (x:xs)) currentLevel accum | x == SGRP = getScore (Stack (x:xs)) currentLevel+1 accum
-                                           | x == EGRP = (Stack xs, (pop accum), top)
-getScore (Stack []) c a = a
-
-run :: [Char] -> Stack Token
-run [] = Stack []
-run l = parse (Stack []) l
+run :: String -> (GroupScore, GarbageCount)
+run str = parse (Stack []) str 0 0 0
 
 -- Takes one commandline argument: filename
 main :: IO()
