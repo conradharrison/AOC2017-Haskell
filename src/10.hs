c46c37079f8a29be3bc8103a8a4278342056aced
Conrad Harrison <conrad.harrison@gmail.com>
2018-03-13 23:31:03 +0530

    Part 1

--- src/10.hs
+++ src/10.hs
@@ -9,22 +9,27 @@ splitOn d s = words (map (\c -> if c `elem` d then ' ' else c) s)
 fromTo :: Int -> Int -> [a] -> [a]
 fromTo from to l = drop from (take (to+1) l)
 
--- current position -> length -> old chain -> new chain
+-- length -> current position -> old chain -> new chain
 runAlgo :: (Show a) => Int -> Int -> [a] -> [a]
 runAlgo x c l | x==0 || x==1 = l
-              | e==c         = let rs = reverse $ (fromTo c (chainLength-1) l) ++ (fromTo 0 (e-1) l) in
-                               (drop (chainLength-c) rs) ++ (take (chainLength-c) rs)
-              | e > c        = take c l ++ (reverse $ fromTo c e l) ++ drop (e+1) l
-              | e < c        = reverse1 ++ (fromTo (e+1) (c-1) l) ++ reverse2
-              where e             = if ((c+x)>chainLength) then (c+x-chainLength) else (c+x-1)
-                    reverse1      = drop (chainLength - c + 1) reverseString
-                    reverse2      = take (e+1) reverseString
-                    reverseString = reverse $ (fromTo c (chainLength-1) l) ++ (fromTo 0 e l)
+              | c+x >= chainLength = runAlgo_wrap x c l
+              | otherwise          = runAlgo_nowrap x c l
+
+-- length -> current position -> old chain -> new chain
+runAlgo_wrap :: (Show a) => Int -> Int -> [a] -> [a]
+runAlgo_wrap x c l = (drop (chainLength - c) rl) ++ (fromTo ((c+x)`mod`chainLength) (c-1) l) ++ (take (chainLength - c) rl)
+                     where rl = reverse $ fromTo c (c+x-1) (l ++ l)
+
+-- length -> current position -> old chain -> new chain
+runAlgo_nowrap :: (Show a) => Int -> Int -> [a] -> [a]
+runAlgo_nowrap x c l = (take c l) ++ rl ++ (drop (c+x) l)
+                       where rl = reverse $ fromTo c (c+x-1) l
+
 
 -- list of lenghts -> current position -> skip size -> old chain -> new chain
 twist :: (Show a) => [Int] -> Int -> Int -> [a] -> [a]
 twist []     _ _ old = old
-twist (x:xs) c s old = twist xs nextc nexts (trace (show (x, c, s, old, new)) new)
+twist (x:xs) c s old = twist xs nextc nexts new
                        where nexts = s+1
                              nextc = (c + x + s) `mod` chainLength
                              new = runAlgo x c old
@@ -42,3 +47,5 @@ main = do
         args <- getArgs
         inStr <- readFile (head args)
         print $ run $ map (\x -> (read x :: Int)) (splitOn ", " inStr)
+
+-- TODO: create a genric circular list
